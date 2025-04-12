d5723270160d8995617b44571ff372b3e0e53cc5
Aniket239 <aniketbiswas2392001@gmail.com>
2025-04-12 23:16:43 +0530

    initial commit

---
+++ src/main/java/com/True/Care/TrueCareApplication.java
@@ -0,0 +1,13 @@
+package com.True.Care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/True/Care/TrueCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.True.Care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
