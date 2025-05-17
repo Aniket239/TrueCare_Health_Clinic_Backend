d0d88c341675ca68fc98efe9d7031582d2d4218c
Aniket239 <aniketbiswas2392001@gmail.com>
2025-05-18 01:48:50 +0530

    doctors api integrated with front end

--- src/main/java/com/True/Care/controller/DoctorsController.java
+++ src/main/java/com/True/Care/controller/DoctorsController.java
@@ -6,17 +6,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.True.Care.helpers.FileUploadHelper;
 import com.True.Care.modal.Departments;
 import com.True.Care.modal.Doctors;
 import com.True.Care.repository.DepartmentsRepository;
@@ -32,13 +39,26 @@ public class DoctorsController {
     @Autowired
     private DepartmentsRepository departmentsRepository;
     Map<String, Object> response = new HashMap<>();
+    Logger logger = LoggerFactory.getLogger(DoctorsController.class);
 
-    @PostMapping(path = "/doctor", consumes = "application/json")
-    public ResponseEntity<Map<String, Object>> addDoctor(@RequestBody Doctors doctor) {
+    @Value("${app.upload.base-url}")
+    private String uploadBaseUrl;
+
+    @PostMapping(path = "/doctor", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Map<String, Object>> addDoctor(@ModelAttribute Doctors doctor, @RequestParam(value = "profileImage", required = false) MultipartFile file) {
+        response.clear();
         try {
             Integer deptId = doctor.getDepartment().getDepartmentId();
             Optional<Departments> department = departmentsRepository.findById(deptId);
             if (department.isPresent()) {
+                if (file != null && !file.isEmpty()) {
+                    String uploadedFileUrl = FileUploadHelper.uploadFile(file, uploadBaseUrl);
+                    logger.info("File uploaded successfully: {}", uploadedFileUrl, file, uploadBaseUrl);
+
+                    if (uploadedFileUrl != null) {
+                        doctor.setProfile_image(uploadedFileUrl);
+                    }
+                }
                 doctorsRespository.save(doctor);
                 response.put("success", true);
                 return ResponseEntity.status(HttpStatus.OK).body(response);
@@ -54,8 +74,9 @@ public class DoctorsController {
         }
     }
 
-    @GetMapping(path = "/doctors/{id}")
+    @GetMapping(path = "/doctor/{id}")
     public ResponseEntity<Map<String, Object>> getDoctor(@PathVariable("id") Integer id) {
+        response.clear();
         try {
             Optional<Doctors> doctor = doctorsRespository.findById(id);
             if (!doctor.isPresent()) {
@@ -88,6 +109,32 @@ public class DoctorsController {
         } catch (Exception e) {
             response.put("success", false);
             response.put("error", "Something went wrong.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+    @GetMapping(path = "/doctors/departmentId")
+    public ResponseEntity<Map<String, Object>> getDoctorsByDepartment(
+            @RequestParam("departmentId") Integer departmentId) {
+        response.clear();
+        try {
+            List<Doctors> doctors = doctorsRespository.findAllByDepartment_DepartmentId(departmentId);
+            logger.info("doctors are ================================== {}", doctors);
+    
+            if (doctors == null || doctors.isEmpty()) {
+                response.put("success", false);
+                response.put("message", "No doctor found in this department");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+    
+            response.put("success", true);
+            response.put("data", doctors);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+    
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("error", "Something went wrong.");
+            response.put("exception", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
     }
--- src/main/java/com/True/Care/repository/DoctorsRespository.java
+++ src/main/java/com/True/Care/repository/DoctorsRespository.java
@@ -1,9 +1,12 @@
 package com.True.Care.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.True.Care.modal.Doctors;
 
 public interface DoctorsRespository extends CrudRepository<Doctors, Integer> {
     boolean existsByDepartment_DepartmentId(Integer departmentId);
+    List<Doctors> findAllByDepartment_DepartmentId(Integer departmentId);
 } 
